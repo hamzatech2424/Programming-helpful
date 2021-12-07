e6a197bbab9e093164ce9886c5b5fda66baddacc
Hamza Shafiq <64663350+hamzatech2424@users.noreply.github.com>
2021-12-07 11:33:02 +0500

    Create BoilerPlateSvg.js

---
+++ BoilerPlateSvg.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {SvgXml} from 'react-native-svg';
+
+
+
+
+
+
+const BoilerPlateSvg = ({height,width,color,size}) => {
+  const defaultHeight = height ? height : 10;
+  const defaultWidth = width ? width : 10
+  const defaultColor = color ? color : 'white'
+  const defaultSize = size ? size : 12
+
+  return (
+    <SvgXml
+      // width={defaultWidth}
+      // height={defaultHeight}
+      xml={` `}
+    />
+  );
+};
+
+
+export default BoilerPlateSvg;
