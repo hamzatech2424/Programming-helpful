f4f0326bbd3b8638fb2d0f541f59a03cd75902ac
Hamza Shafiq <64663350+hamzatech2424@users.noreply.github.com>
2022-03-03 14:13:44 +0500

    Create SearchScreenAspect.js

---
+++ SearchScreenAspect.js
@@ -0,0 +1,181 @@
+import { StyleSheet, Text, View, Dimensions, LayoutAnimation, Platform,UIManager } from 'react-native'
+import React, { useState } from 'react'
+import LinearGradient from 'react-native-linear-gradient'
+import FocusAwareStatusBar from '../../../Components/AbstractComponents/statusbarConfiguration'
+import ScreenContainer from '../../../Components/AbstractComponents/abstractScreenContainer'
+import { Colors, Fonts } from '../../../themes'
+import AbstractStatusBarHeight from '../../../Components/AbstractComponents/abstractStatusBarHeight'
+import AbstractStepsBottomsheet from '../../../Components/AbstractComponents/abstractStepsBottomSheet'
+import { moderateScale } from 'react-native-size-matters'
+import SelectLocation from './selectLocation'
+import SelectType from './selectType'
+import SelectDate from './selectDate'
+
+if (
+    Platform.OS === "android" &&
+    UIManager.setLayoutAnimationEnabledExperimental
+  ) {
+    UIManager.setLayoutAnimationEnabledExperimental(true);
+  }
+
+const SW = Dimensions.get('window').width
+const SH = Dimensions.get('window').height
+
+const HeaderTwo = () => {
+    return (
+        <View style={[styles.viewTwo]}>
+            <View style={[{ width: '55%', marginBottom: 20 }]}>
+                <Text style={styles.textOne}>
+                    What are you looking for?
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+
+const HeaderThree = () => {
+    return (
+        <View style={[styles.viewTwo,]}>
+                <View style={{ width: '55%',marginBottom:20 }}>
+                    <Text style={styles.textOne}>
+                        When will you be there?
+                    </Text>
+                </View>
+        </View>
+    )
+}
+
+const SearchScreen = () => {
+
+    const [firstStep, setFirstStep] = useState(true)
+    const [secondStep, setSecondStep] = useState(false)
+    const [thirdStep, setThirdStep] = useState(false)
+
+
+    const goToSecond = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+        setFirstStep(false)
+        setSecondStep(true)
+        setThirdStep(false)
+    }
+
+    const goToThird = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+        setFirstStep(false)
+        setSecondStep(false)
+        setThirdStep(true)
+    }
+
+    const backToFirst = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+        setFirstStep(true)
+        setSecondStep(false)
+        setThirdStep(false)
+    }
+
+    const backToSecond = () => {
+        LayoutAnimation.configureNext(LayoutAnimation.Presets.spring);
+        setFirstStep(false)
+        setSecondStep(true)
+        setThirdStep(false)
+    }
+
+
+    return (
+        <ScreenContainer
+            upperStyle={{ backgroundColor: Colors.neutralDarkOne }}
+            bottomStyle={{ backgroundColor: Colors.neutralDarkOne }}
+        >
+            <View style={styles.mainConainer}>
+                <LinearGradient
+                    colors={['#C644FC', '#0F73EE']}
+                    useAngle={true}
+                    angle={-90}
+                    angleCenter={{ x: 0.4, y: 0.4 }}
+                    style={[
+                        {
+                            flex: 1,
+                        },
+                    ]}
+                >
+                    <FocusAwareStatusBar
+                        barStyle="light-content"
+                        backgroundColor={'transparent'}
+                        translucent={true}
+                    />
+
+                    <AbstractStatusBarHeight color={'transparent'} />
+
+                    {firstStep ?
+                        <View style={{ flex: 1, backgroundColor: Colors.neutralLightFive, borderTopRightRadius: 25, borderTopLeftRadius: 25 }} >
+                            <SelectLocation onSelect={goToSecond} />
+                        </View>
+                        : false}
+
+                    {secondStep ?
+                               <View style={{position:'absolute',bottom:0,width:'100%'}}>
+                                   <HeaderTwo />
+                                <SelectType onSelect={goToThird} backToFirst={backToFirst} />
+                                </View>
+                        : false}
+
+                    {thirdStep ?
+                        <View style={{ position:'absolute',bottom:0,width:'100%' }} >
+                            <HeaderThree />
+                            <SelectDate backToSecond={backToSecond} />
+                        </View>
+                        : false}
+
+
+
+                </LinearGradient>
+            </View>
+        </ScreenContainer>
+    )
+}
+
+export default SearchScreen
+
+const styles = StyleSheet.create({
+    mainConainer: {
+        flex: 1
+    },
+    viewOne: {
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%',
+        position: 'absolute',
+    },
+    textOne: {
+        fontSize: moderateScale(27, 0.1),
+        fontFamily: Fonts.bold,
+        color: Colors.neutralLightFive
+    },
+    viewTwo: {
+        width: '100%',
+        alignSelf: 'center',
+        width: '90%'
+    },
+    textOne: {
+        fontSize: moderateScale(27, 0.1),
+        fontFamily: Fonts.bold,
+        color: Colors.neutralLightFive
+    },
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <AbstractStatusBarHeight />   
+<AbstractStepsBottomsheet /> */}
